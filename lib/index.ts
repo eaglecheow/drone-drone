0ecf0b52ba7139a544a1af89a74d0bc781853c26
Eagle Cheow <eagle.cheow@hotmail.com>
2019-02-24 14:05:02 +0800

    Modified tcp

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,20 @@
 import * as net from "net";
-import { codeTest } from "./test"
-
+import { codeTest } from "./test";
 
 const PORT = 8080;
 const HOST = "127.0.0.1";
 
-const client = new net.Socket();
-
-client.connect(PORT, HOST, () => {
-    console.log("Client is connected");
-    client.write("Connection established from client");
+const server = net.createServer();
+server.listen(PORT, HOST, () => {
+    console.log(`TCP server running at ${HOST}:${PORT}`);
 });
 
-client.on("data", data => {
-    console.log("Received: ", data);
-    codeTest(data.toString())
-});
+server.on("connection", async sock => {
+    sock.on("data", data => {
+        codeTest(data.toString());
+    });
 
-client.on("close", () => {
-    console.log("Connection closed");
+    sock.on("close", () => {
+        console.log("Socket closed");
+    });
 });
